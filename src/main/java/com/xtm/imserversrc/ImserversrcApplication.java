2b992805cefdc90c1d112ef67721afba08ad3ad8
douguohai <douguohai@163.com>
2021-06-16 11:47:22 +0800

    初始化

---
+++ src/main/java/com/xtm/imserversrc/ImserversrcApplication.java
@@ -0,0 +1,13 @@
+package com.xtm.imserversrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImserversrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImserversrcApplication.class, args);
+    }
+
+}
